6eae758e170ae80fdd20ddab0819d735b4bba16c
xwreach <thetank62200@gmail.com>
2024-03-12 16:10:29 +0100

    new and fresh woooo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"pokemap-1d84d","appId":"1:61804635847:web:fe704a8dc041b5f8f880e2","storageBucket":"pokemap-1d84d.appspot.com","apiKey":"AIzaSyC0oKBEl39UcGjxACK_IJUqErb2ZGWQS1Y","authDomain":"pokemap-1d84d.firebaseapp.com","messagingSenderId":"61804635847"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
